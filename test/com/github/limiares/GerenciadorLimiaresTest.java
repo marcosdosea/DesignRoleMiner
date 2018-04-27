58ef4f7c75d6fd7777e6aa8e44df63531b3da7c8
marcosdosea <marcosdosea@yahoo.com.br>
2018-04-27 11:18:27 -0300

    Separando geração de técnicas que usam bechmark externo das que uso bechmark de projetos referencia.

--- test/com/github/limiares/GerenciadorLimiaresTest.java
+++ test/com/github/limiares/GerenciadorLimiaresTest.java
@@ -11,13 +11,28 @@ import com.github.ck.CKNumber;
 public class GerenciadorLimiaresTest {
 
 	@Test
-	public void testGerarLimiares() {
+	public void testGerarLimiaresReference() {
 		final String PASTA_RESULTADO = "thresholds\\";
 		GerenciadorLimiares gLimiares = new GerenciadorLimiares();
 
 		System.out.println("Iniciando a coleta de métricas do projeto referencia...");
 		ArrayList<String> projetosReferencia = gLimiares.lerProjetos("Reference.txt");
 		ArrayList<CKNumber> metricasProjetosReferencia = gLimiares.getMetricasProjetos(projetosReferencia);
+
+		System.out.println("Gerando Limiares por Dosea Referencia...");
+		gLimiares.gerarLimiarDoseaReference(metricasProjetosReferencia, PASTA_RESULTADO + "R.csv");
+
+		System.out.println("Gerando Limiares por Dosea Referencia e Design Role...");
+		gLimiares.gerarLimiarDoseaReferenceDesignRole(metricasProjetosReferencia, PASTA_RESULTADO + "D.csv");
+
+		System.out.println("Limiares gravados na pasta " + PASTA_RESULTADO + " com sucesso!");
+		assertTrue(true);
+	}
+	
+	//@Test
+	public void testGerarLimiaresBenchmarkExterno() {
+		final String PASTA_RESULTADO = "thresholds\\";
+		GerenciadorLimiares gLimiares = new GerenciadorLimiares();
 
 		System.out.println("Iniciando a coleta de métricas dos projetos WEB...");
 		ArrayList<String> projetosBenchmark = gLimiares.lerProjetos("Benchmark.txt");
@@ -30,16 +45,11 @@ public class GerenciadorLimiaresTest {
 		gLimiares.gerarLimiarVale(metricasProjetosBenchmark, PASTA_RESULTADO + "V.csv");
 
 		System.out.println("Gerando Limiares por Aniche...");
-		//gLimiares.gerarLimiarAniche(metricasProjetosBenchmark, PASTA_RESULTADO + "X.csv");
-
-		System.out.println("Gerando Limiares por Dosea Referencia...");
-		gLimiares.gerarLimiarDoseaReference(metricasProjetosReferencia, PASTA_RESULTADO + "R.csv");
-
-		System.out.println("Gerando Limiares por Dosea Referencia e Design Role...");
-		gLimiares.gerarLimiarDoseaReferenceDesignRole(metricasProjetosReferencia, PASTA_RESULTADO + "D.csv");
-
+		gLimiares.gerarLimiarAniche(metricasProjetosBenchmark, PASTA_RESULTADO + "X.csv");
 
 		System.out.println("Limiares gravados na pasta " + PASTA_RESULTADO + " com sucesso!");
 		assertTrue(true);
 	}
+
+	
 }
