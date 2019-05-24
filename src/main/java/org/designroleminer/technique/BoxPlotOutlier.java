7108d90ed3b5db8f73a454ff6d1be7f0389288d0
marcosdosea <marcosdosea@yahoo.com.br>
2019-05-23 22:34:16 -0300

    boxplot adjusted

--- src/main/java/org/designroleminer/technique/BoxPlotOutlier.java
+++ src/main/java/org/designroleminer/technique/BoxPlotOutlier.java
@@ -1,5 +1,6 @@
 package org.designroleminer.technique;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BoxPlotOutlier {
@@ -41,24 +42,92 @@ public class BoxPlotOutlier {
 			return (listValues[posicaoInteira] + listValues[posicaoInteira - 1]) / 2;
 		}
 	}
-	
+
 	public double getQ1() {
 		return getValuePercentil(25);
 	}
-	
+
 	public double getQ3() {
 		return getValuePercentil(75);
 	}
-	
+
 	public double getIQR() {
 		return getQ3() - getQ1();
 	}
-	
+
 	public double getLowerThresholdTurkey() {
 		return getQ1() - 1.5 * getIQR();
 	}
-	
+
 	public double getUpperThresholdTurkey() {
 		return getQ3() + 1.5 * getIQR();
 	}
+
+
+	
+	public double getLowerThresholdAdjusted() {
+		double medCouple = getMedCouple();
+		double lower = 0;
+		if (medCouple >= 0)
+			lower = getQ1() - 1.5 * Math.exp(-3.5 * medCouple) * getIQR();
+		else
+			lower = getQ1() - 1.5 * Math.exp(-4 * medCouple) * getIQR();
+		return lower;
+	}
+
+	public double getUpperThresholdAdjusted() {
+		double medCouple = getMedCouple();
+		double upper = 0;
+		if (medCouple >= 0)
+			upper = getQ3() + 1.5 * Math.exp(4 * medCouple) * getIQR();
+		else
+			upper = getQ3() + 1.5 * Math.exp(3.5 * medCouple) * getIQR();
+		return upper;
+	}
+
+	
+	/**
+	 * a robust measure of skewness for a skewed distribution .
+	 * 
+	 * @return
+	 */
+	public double getMedCouple() {
+		int endList1 = 0;
+		int startList2 = 0;
+		double median = getMedian();
+
+		if ((listValues.length % 2) == 0) {
+			endList1 = listValues.length / 2;
+			startList2 = listValues.length / 2;
+		} else {
+			endList1 = (listValues.length - 1) / 2;
+			startList2 = (listValues.length + 1) / 2;
+		}
+		ArrayList<Double> listCalculated = new ArrayList<>();
+		for (int i = 0; i < endList1; i++) {
+			for (int j = startList2; j < listValues.length; j++) {
+				double calculated = ((listValues[j] - median) - (median - listValues[i]))
+						/ (listValues[j] - listValues[i]);
+				listCalculated.add(calculated);
+			}
+		}
+
+		double[] target = new double[listCalculated.size()];
+		for (int i = 0; i < target.length; i++) {
+			target[i] = listCalculated.get(i); // java 1.5+ style (outboxing)
+		}
+		Arrays.sort(target);
+
+		int lenghtList = target.length;
+
+		if (lenghtList == 0) {
+			return 0;
+		}
+		if (lenghtList % 2 == 0) {
+			return (target[lenghtList / 2] + target[(lenghtList / 2) - 1]) / 2;
+		} else {
+			return target[(lenghtList - 1) / 2];
+		}
+	}
+
 }
--- test/org/designroleminer/technique/BoxPlotOutlierTest.java
+++ test/org/designroleminer/technique/BoxPlotOutlierTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 public class BoxPlotOutlierTest {
 
 	double[] values = { 2, 5, 7, 11, 15 };
+	double[] values2 = {1, 2, 5, 11, 15, 20};
+	double[] values3 = {1, 2, 3, 4, 5, 6, 7, 10, 15, 16};
 	BoxPlotOutlier boxplot = new BoxPlotOutlier(values);
+	BoxPlotOutlier boxplot2 = new BoxPlotOutlier(values2);
+	BoxPlotOutlier boxplot3 = new BoxPlotOutlier(values3);
+	
 
 	@Test
 	public void testGetMedian() {
-		assertEquals(7, boxplot.getMedian(), 1);
+		assertEquals(7, boxplot.getMedian(), 0);
+		assertEquals(8, boxplot2.getMedian(), 0);
 	}
 
 	@Test
@@ -22,26 +28,50 @@ public class BoxPlotOutlierTest {
 	@Test
 	public void testGetQ1() {
 		assertEquals(5, boxplot.getQ1(), 1);
+		assertEquals(2, boxplot2.getQ1(), 1);
 	}
 
 	@Test
 	public void testGetQ3() {
 		assertEquals(11, boxplot.getQ3(), 1);
+		assertEquals(15, boxplot2.getQ3(), 1);
 	}
 
 	@Test
 	public void testGetIQR() {
 		assertEquals(6, boxplot.getIQR(), 1);
+		assertEquals(13, boxplot2.getIQR(), 1);
 	}
 
 	@Test
 	public void testGetLowerThresholdTurkey() {
-		assertEquals(-4, boxplot.getLowerThresholdTurkey(), 1);
+		assertEquals(-4, boxplot.getLowerThresholdTurkey(), 0);
+		assertEquals(-17.5, boxplot2.getLowerThresholdTurkey(), 0);
+	}
+
+	@Test
+	public void testGetLowerThresholdAdjusted() {
+		assertEquals(1.64, boxplot.getLowerThresholdAdjusted(), 0.1);
+		assertEquals(-12.8, boxplot2.getLowerThresholdAdjusted(), 0.1);
+		assertEquals(0, boxplot3.getLowerThresholdAdjusted(), 0.1);
 	}
 
 	@Test
 	public void testGetUpperThresholdTurkey() {
-		assertEquals(20, boxplot.getUpperThresholdTurkey(), 1);
+		assertEquals(20, boxplot.getUpperThresholdTurkey(), 0);
+		assertEquals(34.5, boxplot2.getUpperThresholdTurkey(), 0);
+	}
+	
+	@Test
+	public void testGetUpperThresholdAdjusted() {
+		assertEquals(38.8, boxplot.getUpperThresholdAdjusted(), 0.1);
+		assertEquals(41.5, boxplot2.getUpperThresholdAdjusted(), 0.1);
+		assertEquals(53.8, boxplot3.getUpperThresholdAdjusted(), 0.1);
+	}
+	
+	@Test
+	public void testGetMedCouple() {
+		assertEquals(0.35, boxplot3.getMedCouple(), 0.1);
 	}
 
 }
